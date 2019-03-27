65a2e0d8c9af6d02f358b5f0acae0255ea0123fd
Kyle D Miller <kmiller@u.northwestern.edu>
2019-03-27 17:17:23 -0500

    tweaked space group reader

--- scripts_dft/space_group_reader.py
+++ scripts_dft/space_group_reader.py
@@ -9,13 +9,13 @@ import sys
 def main():
     files = sys.argv[1:]
     for file in files:
-        print("Symmetry for {} \nsymprec \tSG Sym\tSG Num".format(file))
-        for tol in [0.00001, 0.0001, 0.001, 0.01, 0.1]: 
+        print("\nSymmetry for {} \nprec \tSG Sym\tSG Num".format(file))
+        for tol in [,0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1]: 
             struc = IStructure.from_file(file)
             sg = sga(struc, symprec=tol)
             sgSym = sg.get_space_group_symbol()
             sgNum = sg.get_space_group_number()
-            print("{}\t{}\t{})".format(str(tol),sgSym,str(sgNum)))
+            print("{}\t{}\t{}".format(str(tol),sgSym,str(sgNum)))
 
 if __name__ == "__main__":
     main()
