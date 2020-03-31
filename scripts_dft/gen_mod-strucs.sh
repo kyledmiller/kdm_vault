2706dececa6f85a1292d8966bb6e04b252a6e62a
Kyle Miller (LappyIV) <kyledanielmiller@gmail.com>
2020-03-31 12:39:51 -0500

    tweaked modulation, kpath generator (symprec), adding mod conf files

--- scripts_dft/gen_mod-strucs.sh
+++ scripts_dft/gen_mod-strucs.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 dest=mod-strucs
+sys=BaCoS2
 mkdir $dest
 
-for kpt in 'XX';do
-for idx in 1;do
-for disp in 5 10 15 20 25;do
+for kpt in 'X';do
+for idx in 1;do #INDEX IS 1-BASED, NOT 0-BASED
+for disp in 0 4 8 12 16 20;do
 	conf_file=mod-$kpt".conf"
-	cp ~/files_dft/MgTa2O6/$conf_file .
+	cp ~/files_dft/$sys/$conf_file .
 	sed -i -e "s/disp-flag/$disp/g" $conf_file
 	sed -i -e "s/index-flag/$idx/g" $conf_file
 	phonopy mod-"$kpt".conf
-	mv MPOSCAR $dest/MPOSCAR-"$kpt"-disp$disp.vasp
+	mv MPOSCAR $dest/MPOSCAR-"$kpt$idx"-disp$disp.vasp
 	rm $conf_file
 done
 	mv MPOSCAR-orig $dest/MPOSCAR-"$kpt$idx"-disp0.vasp
--- scripts_dft/kpath.py
+++ scripts_dft/kpath.py
@@ -2,11 +2,18 @@
 # Generates KPOINTS.bands file with high symmetry lines added for
 # an arbitrary POSCAR with symmetry precision=1e-3
 
-import pymatgen as mg
+import pymatgen as pmg
 from pymatgen.symmetry.bandstructure import HighSymmKpath
 from pymatgen.io.vasp.inputs import Kpoints
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
+import sys
 
-struct = mg.Structure.from_file("POSCAR")
-kpath = HighSymmKpath(struct)
+struc_name = sys.argv[1]
+
+struct = pmg.Structure.from_file(struc_name)
+sga = SpacegroupAnalyzer(struct, symprec=1e-5, angle_tolerance=0.1)
+print(f'Found spacegroup {sga.get_space_group_symbol()}')
+struct = sga.get_primitive_standard_structure(international_monoclinic=False)
+kpath = HighSymmKpath(struct, symprec=1e-5, angle_tolerance=0.1)
 kpoints = Kpoints.automatic_linemode(16, kpath)
 kpoints.write_file("KPOINTS.bands")
