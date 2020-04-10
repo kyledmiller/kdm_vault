dd165b79f205ba76b8b4a0fff1141a2e28922be5
Kyle Miller (LappyIV) <kyledanielmiller@gmail.com>
2020-04-10 16:50:57 -0500

    tweaking plots, conversion scripts malfunctioning

--- scripts_dft/convert_poscar_cif.py
+++ scripts_dft/convert_poscar_cif.py
@@ -14,9 +14,9 @@ def main():
         prim = struc.get_primitive_structure(tolerance=0.000001)
         print(f'{file_name},  SG = ' + str(struc.get_space_group_info(symprec=0.000001, angle_tolerance=0.1)))
         if file_name[-5:] == '.vasp':
-            prim.to(fmt='cif', filename=(file_name[:-5]+'.cif'), symprec=0.000001)#, angle_tolerance=0.1)
+            prim.to(fmt='cif', filename=(file_name[:-5]+'.cif'), symprec=0.1, angle_tolerance=1)
         else:
-            prim.to(fmt='cif', filename=(file_name+'.cif'), symprec=0.000001)#, angle_tolerance=0.1)
+            prim.to(fmt='cif', filename=(file_name+'.cif'), symprec=0.000001, angle_tolerance=0.1)
             
 if __name__ == "__main__":
     main()
