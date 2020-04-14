4044c002ee8a679b760f4680cd39dc76ad564b6e
Kyle Miller (LappyIV) <kyledanielmiller@gmail.com>
2020-04-13 20:53:51 -0500

    touching up

--- scripts_dft/gen_mod-strucs.sh
+++ scripts_dft/gen_mod-strucs.sh
@@ -5,18 +5,19 @@ sys=BaCoS2
 mkdir $dest
 dim='2 2 1'
 
-for kpt in 'G0 0 0';do
+#for kpt in 'G0 0 0' 'J0.22 0.22 0';do
+for kpt in 'J0.22 0.22 0';do
 for idx in 1;do #INDEX IS 1-BASED, NOT 0-BASED
-for disp in 0 2 4 6 8 10;do
+for disp in 5 10 15 20;do
 	label=${kpt:0:1}
-	coords=${kpt:1}}
+	coords=${kpt:1}
 	conf_file=mod-$label.conf
-	echo "MODULATION = $dim, $coords, $idx $disp" >> $conf_file
+	echo "MODULATION = $dim, $coords $idx $disp" > $conf_file
 
-	phonopy mod-"$kpt".conf
-	mv MPOSCAR $dest/MPOSCAR-"$kpt$idx"-disp$disp.vasp
+	phonopy mod-"$label".conf
+	mv MPOSCAR $dest/MPOSCAR-"$label$idx"-disp$disp.vasp
 	rm $conf_file
 done
-	mv MPOSCAR-orig $dest/MPOSCAR-"$kpt$idx"-disp0.vasp
+	mv MPOSCAR-orig $dest/MPOSCAR-"$label$idx"-disp0.vasp
 done
 done
