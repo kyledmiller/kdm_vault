a58613e07b41dcd18270bfc33e4ea759edd01e14
Kyle Miller (LappyIV) <kyledanielmiller@gmail.com>
2020-05-05 21:26:40 -0500

    updated/consolidated extract stats

--- scripts_dft/extract_stats-static.sh
+++ scripts_dft/extract_stats-static.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 
-writeDir=output_files
 writeFile=stats.tsv
 
 echo "ISPIN =" 
 read ISPIN
 
-mkdir $writeDir
-
-#for outfile in $(find . -wholename '*/final-OUTCAR' |sort);
-if [ $ISPIN -eq 2 ];then
-	printf "Total Energy\tDrift in x\tDrift in y\tDrift in z\tVolume\tMag s\t Mag p \t Mag d\t Mag tot\n" >> $writeFile
-else
-	printf "Total Energy\tDrift in x\tDrift in y\tDrift in z\tVolume\n" >> $writeFile
-fi
-
-
-outfile=OUTCAR
-grep TOTEN $outfile | tail -1 | awk '{printf $5; exit}' >> $writeFile
-tac $outfile | awk '/drift/ {printf "\t"$3; exit}' >> $writeFile 
-tac $outfile | awk '/drift/ {printf "\t"$4; exit}' >> $writeFile 
-tac $outfile | awk '/drift/ {printf "\t"$5; exit}' >> $writeFile
 
 if [ $ISPIN -eq 2 ];then
-        tac $outfile | awk '/volume/ {printf "\t"$5; exit}' >> $writeFile
-        tac $outfile | awk '/tot     / {printf "\t"$2; exit}' >> $writeFile
-        tac $outfile | awk '/tot     / {printf "\t"$3; exit}' >> $writeFile
-        tac $outfile | awk '/tot     / {printf "\t"$4; exit}' >> $writeFile
-        tac $outfile | awk '/tot     / {printf "\t"$5"\n"; exit}' >> $writeFile
-	
+        printf "run_id\tenergy\tvolume\tmag_s\tmag_p\tmag_d\tmag_tot\tpoint_group\tlattice\n" >> $writeFile
 else
-	tac $outfile | awk '/volume/ {printf "\t"$5"\n"; exit}' >> $writeFile
+        printf "run_id\tenergy\tvolume\tpoint_group\tlattice\n" >> $writeFile
 fi
-	
-cp $writeFile $writeDir
+
+while [ "$1" != "" ]; do
+	outfile="$1/static/OUTCAR"
+	printf "$1" >> $writeFile
+	echo $outfile
+	grep TOTEN $outfile | tail -1 | awk '{printf "\t"$5; exit}' >> $writeFile
+
+        if [ $ISPIN -eq 2 ];then
+                tac $outfile | awk '/volume/ {printf "\t"$5; exit}' >> $writeFile
+                tac $outfile | awk '/tot     / {printf "\t"$2; exit}' >> $writeFile
+                tac $outfile | awk '/tot     / {printf "\t"$3; exit}' >> $writeFile
+                tac $outfile | awk '/tot     / {printf "\t"$4; exit}' >> $writeFile
+                tac $outfile | awk '/tot     / {printf "\t"$5; exit}' >> $writeFile
+ 		tac $outfile | awk '/static configuration/ {printf "\t"$8; exit}' >> $writeFile
+ 		tac $outfile | awk '/LATTYP/ {printf "\t"$5"\n"; exit}' >> $writeFile
+	else
+        	tac $outfile | awk '/volume/ {printf "\t"$5; exit}' >> $writeFile
+ 		tac $outfile | awk '/static configuration/ {printf "\t"$8; exit}' >> $writeFile
+ 		tac $outfile | awk '/LATTYP/ {printf "\t"$5"\n"; exit}' >> $writeFile
+	fi
+	shift
+done
