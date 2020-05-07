87079147481f0c4425e3bf0c9e6640ed613efae8
Kyle Miller (LappyIV) <kyledanielmiller@gmail.com>
2020-05-07 11:07:28 -0500

    updating gen_mod-strucs for phonopy 2.6

--- scripts_dft/gen_mod-strucs.sh
+++ scripts_dft/gen_mod-strucs.sh
@@ -3,20 +3,24 @@
 dest=mod-strucs
 sys=BaCoS2
 mkdir $dest
-dim='2 2 1'
+mod_dim='1 1 1'
+scell_dim='2 2 1'
 
-#for kpt in 'G0 0 0' 'J0.22 0.22 0';do
-for kpt in 'J0.22 0.22 0';do
+#for kpt in 'G0 0 0' 'X0 0.5 0';do
+for kpt in 'G0 0 0';do
 for idx in 1;do #INDEX IS 1-BASED, NOT 0-BASED
-for disp in 5 10 15 20;do
+#idx=G
+for disp in 0 1 5 10 15;do
 	label=${kpt:0:1}
 	coords=${kpt:1}
 	conf_file=mod-$label.conf
-	echo "MODULATION = $dim, $coords $idx $disp" > $conf_file
+	echo "MODULATION = $mod_dim, $coords $idx $disp" > $conf_file
+	#echo "MODULATION = $mod_dim, $coords 1 $disp, $coords 2 $disp" > $conf_file
 
-	phonopy mod-"$label".conf
+	phonopy --dim="$scell_dim" mod-"$label".conf
 	mv MPOSCAR $dest/MPOSCAR-"$label$idx"-disp$disp.vasp
 	rm $conf_file
+	rm MPOSCAR-*
 done
 	mv MPOSCAR-orig $dest/MPOSCAR-"$label$idx"-disp0.vasp
 done
