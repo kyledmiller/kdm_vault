ecc00f1b9e294f90254d731949198954534fda53
Kyle Miller (LappyIV) <kyledanielmiller@gmail.com>
2020-06-03 20:40:45 -0500

    revamped phonon fetch, conversion, plotting, added script for transforming/making supercells on the fly

--- scripts_dft/convert-transform-struc_poscar.py
+++ scripts_dft/convert-transform-struc_poscar.py
@@ -8,16 +8,21 @@ from pymatgen.core.structure import Structure
 import sys
 
 def main():
-    SYMPREC = 1e-5
+    SYMPREC = 1e-4
     ANGLE_TOL = 0.1
     file_names = sys.argv[1:]
     for file_name in file_names:
         struc = Structure.from_file(file_name)
         print(f'{file_name},  SG = ' + str(struc.get_space_group_info(symprec=SYMPREC, angle_tolerance=ANGLE_TOL)))
+        
+        struc.make_supercell([[1,1,0],[-1,1,0],[0,0,1]])
+
         if file_name[-5:] == '.vasp':
-            struc.to(fmt='cif', filename=(file_name[:-5]+'.cif'), symprec=SYMPREC, angle_tolerance=ANGLE_TOL)
+            struc.to(fmt='poscar', filename=(file_name[:-5] + '.vasp'))
+        elif file_name[-4:] == '.cif':
+            struc.to(fmt='poscar', filename=(file_name[:-4] + '.vasp'))
         else:
-            struc.to(fmt='cif', filename=(file_name+'.cif'), symprec=SYMPREC, angle_tolerance=ANGLE_TOL)
-            
+            struc.to(fmt='poscar', filename=(file_name + '.vasp'))
+
 if __name__ == "__main__":
     main()
--- scripts_dft/fetch_phon-collective-outputs.sh
+++ scripts_dft/fetch_phon-collective-outputs.sh
@@ -1,12 +1,14 @@
-loc='qaddr:/projects/b1027/KDMiller_work/BaCoS2/correct_mag/Pba2-phonons'
+loc='qaddr:/projects/b1027/KDMiller_work/BaCoS2/correct_mag/phonons'
 
 #for iter in U0 U2 U4;do
-for iter in 'U2';do
+for iter in 'prim-tet-U2';do
 	mkdir $iter
 	cd $iter
 	scp $loc/$iter/FORCE_SETS .
 	scp $loc/$iter/band.yaml .
 	scp $loc/$iter/POSCAR .
 	scp $loc/$iter/phonopy_disp.yaml .
+	scp $loc/$iter/BORN .
+	scp $loc/$iter/SPOSCAR .
 	cd ..	
 done
