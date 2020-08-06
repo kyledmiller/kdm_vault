51c4cfbd43ec4b65497f6470f7830a51518cabf1
Kyle Miller (LappyIV) <kyledanielmiller@gmail.com>
2020-08-05 20:20:55 -0500

    added script for cif --> poscar conversion, renaming

---
+++ scripts_dft/convert_cif_poscar.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Kyle Miller
+"""
+
+from pymatgen.core.structure import Structure
+import sys
+
+def main():
+    SYMPREC = 1e-6
+    ANGLE_TOL = 0.01
+    file_names = sys.argv[1:]
+    for file_name in file_names:
+        struc = Structure.from_file(file_name)
+        print(f'{file_name},  SG = ' + str(struc.get_space_group_info(symprec=SYMPREC, angle_tolerance=ANGLE_TOL)))
+        if file_name[-4:] == '.cif':
+            struc.to(fmt='poscar', filename=(file_name[:-4]+'.vasp'))
+        else:
+            struc.to(fmt='poscar', filename=(file_name+'.vasp'))
+            
+if __name__ == "__main__":
+    main()
