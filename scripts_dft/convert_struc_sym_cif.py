87b72b24ace32b325cafb9e78a55630c1ab7418d
Kyle Miller (LappyIV) <kyledanielmiller@gmail.com>
2023-09-20 11:02:50 -0500

    updated struc sym to accurately print SG

--- scripts_dft/convert_struc_sym_cif.py
+++ scripts_dft/convert_struc_sym_cif.py
@@ -10,8 +10,8 @@ from pymatgen.io.cif import CifWriter
 import sys
 
 def main():
-    SYMPREC = 1e-5
-    ANGLE_TOL = 0.1
+    SYMPREC = 1e-1
+    ANGLE_TOL = 1
     file_names = sys.argv[1:]
     for file_name in file_names:
         struc = Structure.from_file(file_name)
@@ -19,9 +19,10 @@ def main():
         #sga = SpacegroupAnalyzer(struc)
         #prim = sga.get_symmetrized_structure()
         #prim = struc.get_primitive_structure(tolerance=SYMPREC)
-        print(f'{file_name},  SG = ' + str(struc.get_space_group_info(symprec=SYMPREC, angle_tolerance=ANGLE_TOL)))
         out_file_name = file_name.replace('.vasp','').replace('.cif','') + '_sym.cif'
         cw.write_file(out_file_name)
+        struc = Structure.from_file(out_file_name)
+        print(f'{file_name},  SG = ' + str(struc.get_space_group_info(symprec=SYMPREC, angle_tolerance=ANGLE_TOL)))
             
 if __name__ == "__main__":
     main()
