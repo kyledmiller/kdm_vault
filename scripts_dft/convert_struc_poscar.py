64d313c999a0c99972be141f32a5414b1dce596d
Kyle D Miller <kmiller@u.northwestern.edu>
2024-03-14 16:42:33 -0500

    avoid overwrite

--- scripts_dft/convert_struc_poscar.py
+++ scripts_dft/convert_struc_poscar.py
@@ -6,15 +6,17 @@
 
 from pymatgen.core.structure import Structure
 import sys
+import os
 
 def main():
-    SYMPREC = 1e-6
-    ANGLE_TOL = 0.1
+    SYMPREC = 1
+    ANGLE_TOL = 5
     file_names = sys.argv[1:]
     for file_name in file_names:
         struc = Structure.from_file(file_name)
         print(f'{file_name},  SG = ' + str(struc.get_space_group_info(symprec=SYMPREC, angle_tolerance=ANGLE_TOL)))
         out_file_name = file_name.replace('.vasp','').replace('.cif','') + '.vasp'
+        if os.path.exists(out_file_name): print('Renaming file to avoid overwrite'); out_file_name = 'transf_'+out_file_name
         struc.to(fmt='poscar', filename=out_file_name)
             
 if __name__ == "__main__":
